9f5d6e96f2999ae8f81b8bb29adda381c9144e21
AniKanayo <micanian4real@gmail.com>
2023-08-01 22:17:46 +0000

    0x02. i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""This Module is for Flask application"""
+"""Module for Flask application"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world() -> str:
     """Returns index.html when route '/' is requested"""
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
