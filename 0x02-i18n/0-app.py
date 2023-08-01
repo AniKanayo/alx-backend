719b371568ff49b948847d39d765b64fde78879b
AniKanayo <micanian4real@gmail.com>
2023-08-01 23:02:08 +0000

    0x02. i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
-"""
-0. Basic Flask App
+"""A basic Flask application.
 """
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index() -> str:
-    """Route handler for the index page."""
-    return render_template('index.html', title="Welcome to Holberton",
-                            header="Hello world")
+    """The main route which will display 'Hello, World'.
+    Returns:
+        str: The HTML template to display.
+    """
+    return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
